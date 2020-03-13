3dd563e72819b812f120b09016261d61bf051226
ronaldjavier <ronald2807@hotmail.com>
2020-03-13 16:20:28 -0300

    Active Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get 'completed_points/index'
   resources :points
   devise_for :users, controllers: { 
