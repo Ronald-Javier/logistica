9b53cb7c55f3d0350deef41d12274e5321b603a4
ronaldjavier <ronald2807@hotmail.com>
2020-03-20 03:53:47 -0300

    Finished

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,41 +1,41 @@
 ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
-
+  
   content title: proc { I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
-    
+      
     end
     columns do
       column do
         panel "Últimos Puntos completados" do
-        ul do
-        CompletedPoint.last(5).map do |point|
-        li link_to(point.point.name, admin_completed_points_path)
+          ul do
+            CompletedPoint.last(5).map do |point|
+              li link_to(point.point.name, admin_completed_points_path)
+            end
+          end
         end
-        end
-        end
-        end
-      column do
-      panel "Usuarios" do
-      ul do
-      li "Usuarios registrados: #{User.count}"
-      li "Administradores registrados: #{AdminUser.count}"
-      end
-      end
       end
       column do
-      panel "Puntos" do
-      ul do
-      li "Puntos creados: #{Point.count}"
-      li "Puntos completados: #{CompletedPoint.count}"
-      end
-      end
-      end
-      end
-      panel "Gráfico" do
-        render 'shared/chart'
+        panel "Usuarios" do
+          ul do
+            li "Usuarios registrados: #{User.count}"
+            li "Administradores registrados: #{AdminUser.count}"
+          end
         end
-
+      end
+      column do
+        panel "Puntos" do
+          ul do
+            li "Puntos creados: #{Point.count}"
+            li "Puntos completados: #{CompletedPoint.count}"
+          end
+        end
+      end
+    end
+    panel "Gráfico" do
+      render 'shared/chart'
+    end
+    
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
@@ -48,14 +48,14 @@ ActiveAdmin.register_page "Dashboard" do
     #       end
     #     end
     #   end
-
+    
     #   column do
     #     panel "Info" do
     #       para "Welcome to ActiveAdmin."
     #     end
     #   end
     # end
-    end
-
-  end 
+  end
+  
+end 
 
